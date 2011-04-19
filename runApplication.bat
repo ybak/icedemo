41fbefee5063a141c12e81c91a416fc841116c69
ybakswu <ybakswu@gmail.com>
2011-04-19 09:20:22 +0800

    add shell scripts skeleton

---
+++ runApplication.bat
@@ -0,0 +1,2 @@
+icegridadmin --Ice.Config=src\main\resources\config.grid -e "application remove Simple"
+cmd /k icegridadmin --Ice.Config=src\main\resources\config.grid -e "application add 'src\main\resources\application.xml'"
